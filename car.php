7270048da7720f1db9bf0d7680c6c7173a567837
Lym-007 <1733236199@qq.com>
2018-09-28 10:55:46 +0800

    购物车0.0.1

--- car.php
+++ car.php
@@ -1,6 +1,6 @@
 <?php
     header("Content-type:text/html;charset=utf-8");
-    $info=array("id"=>$_GET["id"],"name"=>$_GET["name"],"price"=>$_GET["price"]);
+    $info=array("id"=>$_GET["id"],"name"=>$_GET["name"],"number"=>$_GET["number"],"price"=>$_GET["price"]);
     $info=json_encode($info);//转为json字符串
     echo $info;
 ?>
